fbbc3f04bd8cd05334429e6e0efd0096f44042b9
Vic24Rich <prch.victor.9006@gmail.com>
2026-09-10 19:54:22 -0300

    Aula 10/09

--- ifitness-ui/src/app/app.routes.ts
+++ ifitness-ui/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { ActivitiesListComponent } from './activities/activities-list/activities-list.component';
 import { LoginFormComponent } from './security/login-form/login-form.component';
 import { ActivityRegisterComponent } from './activities/activity-register/activity-register.component';
+import { UserRegisterComponent } from './users/user-register/user-register.component';
 
 export const routes: Routes = [
   { path: 'activities/:id', component: ActivityRegisterComponent },
   { path: 'activities', component: ActivitiesListComponent },
   { path: 'activities/new', component: ActivityRegisterComponent },
+  { path: 'users/new', component: UserRegisterComponent },
   { path: 'login', component: LoginFormComponent }
 ];
